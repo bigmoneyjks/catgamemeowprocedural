fcdc6ddee0bc3ba3942172fb281422084857f258
bigmoneyjks <jokubas.kincinas@laetottenham.ac.uk>
2024-11-05 10:31:06 +0000

    finished game

--- main.py
+++ main.py
@@ -1,32 +1,47 @@
 cat_attributes = {
-    "intelligence": 0,
-    "energy": 0,
-    "weight": 0,
-    # change the inital values above
+    "intelligence": 3,
+    "energy": 20,
+    "weight": 4,
 }
 
 print("Welcome to my cat game!")
 
 # Take the user inputs for name and colour:
-name = input("Enter name:")
-# ...
+name = input("Enter name of cat: ")
+colour = input("Enter the colour of your cat: ")
 
 # start the while loop
 while True:
     # Finish the string below
-    option = input("What would you like to do? 1. Play with your cat 2. Train your cat 3. show stats")
+    option = input("What would you like to do? 1. Play with your cat 2. Train your cat 3. Feed cat 4. Make them sleep 5. Show stats: ")
 
-    if option == '1':
-        # change the cat's attributes here
-        pass
-    elif option == '2':
-        pass
-    # elif ...
-    else:
-        pass
+    if option == '1' and cat_attributes["energy"] >= 3:
+        print("Your cat decices to sprint around the room")
+        cat_attributes["energy"] -= 3
+        cat_attributes["weight"] -= 0.2
+    elif option == '2' and cat_attributes["energy"] >= 7:
+        print("Your cat reads a novel whilst lifting a dumbell")
+        cat_attributes["energy"] -= 7
+        cat_attributes["weight"] += 0.3
+        cat_attributes["intelligence"] += 0.3
+    elif option == "3":
+        print("Your cat eats a 3 course exotic meal")
+        cat_attributes["energy"] += 4
+        cat_attributes["weight"] += 0.1
+    elif option == "4":
+        print("Your cat curls up and snores loudly")
+        cat_attributes["energy"] += 10
+    elif option == "5":
+        print(cat_attributes)
 
     # finish off the if statements below
-    if cat_attributes['energy'] < 0:
-        pass
-    # elif ...
+    if cat_attributes['energy'] <= 0:
+        print("Your cat has passed out since they were so tired")
+        cat_attributes["energy"] = 15
+    elif cat_attributes["weight"] >= 8:
+        print("Your cat has died from being too fat")
+        break
+    elif cat_attributes["weight"] <= 1:
+        print("Your cat as died from being too malnourished")
+        break
     
